019243e3781ab0e7b047e309944523d416f64467
Satyam <thesatyam10@gmail.com>
2020-09-06 21:36:08 +0530

    Seprate all the files SI, average, maxmin and sum

---
+++ SI.py
@@ -0,0 +1,5 @@
+# function of simple interest
+
+def SimpleInterest(P,R,T):
+    SI = P*R*T/100
+    return SI
---
+++ sum.py
@@ -0,0 +1,7 @@
+# function of sum
+
+def SUM(x):
+    sum2 = 0
+    for i in x:
+        sum2 = sum2+i
+    return sum2
